555533ce649378940e22673d0a6139dbc3b74e6e
ArunKumar_D <arunkumard1018@gmail.com>
2024-01-24 20:02:02 +0530

    improved Navbar options and user settings options'

--- myblog-app/src/components/NavBar.jsx
+++ myblog-app/src/components/NavBar.jsx
@@ -1,19 +1,106 @@
-import React from 'react';
+import React, { useState } from 'react';
 import humberger from "../assets/icons/humberger.svg";
-
+import search from "../assets/icons/search.svg";
+import closeIcon from "../assets/icons/close-svgrepo-com.svg";
+import user_icon from "../assets/icons/user.svg";
+import writeIcon from "../assets/icons/write-svgrepo-com.svg";
+import userImage from "../assets/images/logo512.png"
+import '../App.css';
 function NavBar() {
+
+    const [isMobileMenuOpen, setisMobileMenuOpen] = useState(false)
+    const [isUserMenuOpen, setisUserMenuOpen] = useState(false)
+    // need to replace once auth context is created
+    const [isAuthenticated, setisAuthenticated] = useState(false)
+
+    const toogleMobileMenuOpen = () => {
+        setisMobileMenuOpen(!isMobileMenuOpen)
+    }
+
+
     return (
-        <div className="NavBar bg-white items-center">
-            <nav className='flex justify-between items-center mx-4 h-16'>
+        <div className="NavBar  items-center ">
+            <nav className='flex justify-between items-center mx-4 h-16 md:mx-24'>
                 <div className="nav-left">
-                    <h1 className='text-SaintPetersburgDarkBlue font-bold text-2xl' >My Blog</h1>
+                    <h1 onClick={() => setisAuthenticated(!isAuthenticated)} className='text-SaintPetersburgDarkBlue font-bold text-2xl cursor-pointer' >My Blog</h1>
                 </div>
-                <div className="nav-right">
-                    <div className="humberger">
-                        <img src={humberger} alt="icon" />
+
+                <div className="nav-right flex">
+                    <div className="hidden search md:flex border px-2 rounded-lg mr-16">
+                        <input className='outline-none py-2 px-2 bg-transparent w-[20rem]' placeholder='Search...' type="text" />
+                        <button><img src={search} alt="" className='rotate-90 invert' /></button>
+                    </div>
+
+                    <div className="hidden md:flex   items-center">
+                        {isAuthenticated && (
+                            <div className='flex space-x-16 mr-16 text-white text-xl ' >
+                                <div className='hover:underline'><a href="/">Home</a></div>
+                                <div className='hover:underline'><a href="/">About us</a></div>
+                                <a href="/" className='hover:underline'>
+                                    <div className='flex space-x-1'>
+                                        <div>Write</div>
+                                        <img className='w-5 invert' src={writeIcon} alt="write" />
+                                    </div>
+                                </a>
+                            </div>
+                        )}
+                        {isAuthenticated && <div onClick={() => setisUserMenuOpen(!isUserMenuOpen)} className='border rounded-full cursor-pointer'><img className='invert p-3 rounded-full' src={user_icon} alt="UserIcon" /></div>}
+                        {!isAuthenticated && <button className=" border hover:border-SaintPetersburgDarkBlue py-2 px-6 rounded-lg hover:bg-SaintPetersburgLightBlue">Sign in</button>}
+                    </div>
+                    {isUserMenuOpen && (
+                        <div className="hidden md:flex dropdown-menu absolute right-[7rem] mt-11 w-64 bg-transparent border rounded-md shadow-md">
+                            <ul className="py-2 space-y-2 mx-4 text-white mt-2">
+                                <div className="user flex items-center justify-between space-x-2">
+                                    <div className="profile w-16 rounded-full"><img src={userImage} alt="" /></div>
+                                    <div className="userdetails space-y-1">
+                                        <h1>User Name</h1>
+                                        <h1>user@gmail.com</h1>
+                                    </div>
+                                </div>
+                                <li><a href="/">Settings</a></li>
+                                <li><a href="/">Saved Post</a></li>
+                                <li><a href="/">Membership</a></li>
+                                <li><a href="/">Logout</a></li>
+                            </ul>
+                        </div>
+                    )}
+
+                    <div className="md:hidden humberger mr-2">
+                        {!isMobileMenuOpen && (<img onClick={toogleMobileMenuOpen} className='w-6' src={humberger} alt="icon" />)}
+                        {isMobileMenuOpen && (<img onClick={toogleMobileMenuOpen} className='w-8' src={closeIcon} alt="icon" />)}
                     </div>
                 </div>
             </nav>
+            {isMobileMenuOpen && (
+                <div className="MobileMenu md:hidden
+                fixed top-12 right-0 h-screen w-1/2 bg-transparent transform translate-x-0 transition-transform ease-in-out duration-500"  >
+                    {!isAuthenticated && (
+                        <div className='flex flex-col items-end space-y-3 pr-9 pt-8 text-xl'>
+                            <div onClick={toogleMobileMenuOpen}><a href="/">Home</a></div>
+                            <div onClick={toogleMobileMenuOpen}><a href="/">About</a></div>
+                            <div onClick={toogleMobileMenuOpen}><a href="/">Sign in</a></div>
+                        </div>
+                    )}
+                    {isAuthenticated && (
+                        <div className='flex flex-col items-end space-y-3 pr-9 pt-8 text-xl'>
+                            <div onClick={toogleMobileMenuOpen}><a href="/">Profile</a></div>
+                            <a href="/">
+                                <div className='flex space-x-1'>
+                                    <div>Write</div>
+                                    <img className='w-5 ' src={writeIcon} alt="write" />
+                                </div>
+                            </a>
+                            <div onClick={toogleMobileMenuOpen}><a href="/">Saved Post</a></div>
+                            <div onClick={toogleMobileMenuOpen}><a href="/">Membership</a></div>
+                            <div onClick={toogleMobileMenuOpen}><a href="/">Sign Out</a></div>
+                        </div>
+
+
+                    )}
+
+
+                </div>
+            )}
         </div>
     )
 }
