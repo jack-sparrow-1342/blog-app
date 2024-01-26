6c98b659ab1f32a8a2e5b6e46b19563d7cdf7a78
ChiragPN <chiragchiru419@gmail.com>
2024-01-27 03:53:54 +0530

    Implemented HeroComponent.jsx

--- myblog-app/src/pages/BlogPostList.jsx
+++ myblog-app/src/pages/BlogPostList.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import BlogPost from '../components/BlogPost';
 import { blogPosts } from '../api/BlogPostService'
+import HeroComponent from '../components/HeroComponent';
 
 function BlogPostList() {
 
     return (
-        <div className='md:mx-7 bg-transparent'>
-            <div className="flex ">
-                <div className='flex flex-wrap'>
-                    {blogPosts.map((post) => (
-                        <BlogPost post={post}  key={post.id}/>
-                    ))}
+        <>
+            <HeroComponent/>
+            <div className='md:mx-20 bg-transparent'>
+                <div className="flex flex-wrap">
+                    <div className="grid grid-cols-1 md:grid md:grid-cols-2 lg:grid-cols-3">
+                        {blogPosts.map((post) => (
+                            <BlogPost post={post} key={post.id}/>
+                        ))}
+                    </div>
                 </div>
-
             </div>
-        </div>
+        </>
     )
 }
 
